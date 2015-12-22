b7334bd69674c1cef0efe5665feecc005b066016
Pippin Williamson <pippin@pippinsplugins.com>
2015-12-22 14:04:24 -0600

    Query pippinsplugins.com for the license key

--- rcp-add-on-version-response.php
+++ rcp-add-on-version-response.php
@@ -44,26 +44,32 @@ class RCP_Add_On_Version_Response {
 			return;
 		}
 
-		$licensing = edd_software_licensing();
-		$license   = $licensing->get_license_by_key( sanitize_text_field( $_POST['license'] ) );
+		$license = sanitize_text_field( $_POST['license'] );
+		$url     = sanitize_text_field( $_POST['url'] );
+		$valid   = $this->check_license( $license, $url );
 
-		if( ! $license ) {
-			status_header( 402 );
-			return;
+		if( empty( $valid ) ) {
+			status_header( 401 );
+			return; // Not a developer license
 		}
-		
-		$price_id = (int) get_post_meta( $license, '_edd_sl_download_price_id', true );
 
-		if( 2 !== $price_id && 3 !== $price_id ) {
+		if( empty( $valid->success ) || (int) $valid->license_limit !== 0 ) {
 			status_header( 401 );
 			return; // Not a developer license
 		}
 
 		// All good, retrieve the Add On details
 
-		if( 'expired' === $licensing->get_license_status( $license ) ) {
+		if( 'expired' === $valid->license ) {
+
 			$description = '<p><strong>' . __( 'Your license is expired. Please renew it or purchase a new one in order to update this item.', 'edd_sl' ) . '</strong></p>' . $description;
 			$changelog   = '<p><strong>' . __( 'Your license is expired. Please renew it or purchase a new one in order to update this item.', 'edd_sl' ) . '</strong></p>' . $changelog;
+
+		} elseif( 'disabled' === $valid->license ) {
+
+			$description = '<p><strong>' . __( 'Your license key has been disabled.', 'edd_sl' ) . '</strong></p>' . $description;
+			$changelog   = '<p><strong>' . __( 'Your license key has been disabled.', 'edd_sl' ) . '</strong></p>' . $changelog;
+
 		} else {
 
 			$changelog   = get_post_meta( $add_on->ID, '_edd_sl_changelog', true );
@@ -74,7 +80,7 @@ class RCP_Add_On_Version_Response {
 		$response = array(
 			'new_version'   => get_post_meta( $add_on->ID, '_edd_sl_version', true ),
 			'name'          => $add_on->post_title,
-			'slug'          => $_POST['slug'],
+			'slug'          => sanitize_text_field( $_POST['slug'] ),
 			'url'           => get_permalink( $add_on->ID ),
 			'homepage'      => get_permalink( $add_on->ID ),
 			'package'       => $this->get_encoded_download_package_url( $add_on->ID ),
@@ -89,6 +95,35 @@ class RCP_Add_On_Version_Response {
 
 		echo json_encode( $response ); exit;
 
+	}
+
+	public function check_license( $license_key = '', $url = '' ) {
+
+		$params = array(
+			'license'    => $license_key,
+			'edd_action' => 'check_license',
+			'item_id'    => 7460,
+			'url'        => $url
+		);
+
+		$args = array(
+			'timeout'   => 15,
+			'sslverify' => false
+		);
+
+		$response = wp_remote_get( add_query_arg( $params, 'https://pippinsplugins.com' ), $args );
+
+		if( is_wp_error( $response ) ) {
+			return false;
+		}
+
+		$body = wp_remote_retrieve_body( $response );
+
+		if( $body ) {
+			return json_decode( $body );
+		}
+
+		return false;
 	}
 
 	public function get_encoded_download_package_url( $add_on_id ) {
